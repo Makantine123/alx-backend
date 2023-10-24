df908a1afc432ca875404b8358dad5c53a606a07
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-25 01:11:49 +0200

    Commit

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -27,4 +27,6 @@ class LRUCache(BaseCaching):
         """Returns self.cache_data data for the key"""
         if key is None or key not in self.cache_data:
             return None
-        return self.cache_data[key]
+        item = self.cache_data.pop(key)
+        self.cache_data[key] = item
+        return item
