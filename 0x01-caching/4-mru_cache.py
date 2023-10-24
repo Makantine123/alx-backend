ed14d4694a98b49db0739cf175a19a1efc62788a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-25 01:21:03 +0200

    Commit

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -26,4 +26,6 @@ class MRUCache(BaseCaching):
         """Returns self.cache_data data for the key"""
         if key is None or key not in self.cache_data:
             return None
-        return self.cache_data[key]
+        item = self.cache_data.pop(key)
+        self.cache_data[key] = item
+        return item
