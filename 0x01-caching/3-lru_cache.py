bf4814b7d73b7d4886fe06055bde79aa6f4959c6
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-25 01:02:39 +0200

    Commit

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""LRUCache Module"""
+
+from base_caching import BaseCaching
+from collections import deque
+
+
+class LRUCache(BaseCaching):
+    """LRUCache Class"""
+
+    def __init__(self):
+        """Initialisation of class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigns the dictionary self.cache_data the key and its value"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_items = list(self.cache_data.keys())
+            discarded_key = lru_items[0]
+            self.cache_data.pop(discarded_key)
+            print(f"DISCARD: {discarded_key}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns self.cache_data data for the key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
